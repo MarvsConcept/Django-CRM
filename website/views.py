4528d985b89d64b226b9945d48059821e6928e75
MarvsConcept <marvade8@gmail.com>
2023-05-14 18:47:19 +0100

    Setting up the CRM

--- website/views.py
+++ website/views.py
@@ -2,9 +2,11 @@ from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
 from .forms import SignUpForm
-
+from .models import Record
 # Create your views here.
 def home(request):
+    records = Record.objects.all()
+    
     # Check to see if logging in
     if request.method == 'POST':
         username = request.POST['username']
@@ -19,7 +21,7 @@ def home(request):
             messages.success(request, "There was an error logging in, Please try again...")
             return redirect('Home')
     else:
-        return render(request, 'home.html', {})
+        return render(request, 'home.html', {'records':records})
 
 
 def logout_user(request):
@@ -44,5 +46,16 @@ def register_user(request):
         return render(request, 'register.html', {'form':form})
     
     return render(request, 'register.html', {'form':form})
+
+
+def customer_record(request, pk):
+    if request.user.is_authenticated:
+        #Look Up Records
+        customer_record = Record.objects.get(id=pk)
+        return render(request, 'record.html',)
+    else:
+        messages.success(request, "You must be logged in to view that page!")
+        return redirect("Home")
+    
 
 
