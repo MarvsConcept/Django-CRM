b8e025a9d91a3908608b4b3cdc0ffb260b47329b
MarvsConcept <marvade8@gmail.com>
2023-05-15 23:42:32 +0100

    Adding Cards with Bootstrap and also Setting up the Back and Delete button

--- website/views.py
+++ website/views.py
@@ -52,10 +52,18 @@ def customer_record(request, pk):
     if request.user.is_authenticated:
         #Look Up Records
         customer_record = Record.objects.get(id=pk)
-        return render(request, 'record.html',)
+        return render(request, 'record.html', {'customer_record':customer_record})
     else:
         messages.success(request, "You must be logged in to view that page!")
         return redirect("Home")
     
-
+def delete_record(request, pk):
+    if request.user.is_authenticated:
+        delete_it = Record.objects.get(id=pk)
+        delete_it.delete()
+        messages.success(request, "Record Deleted Successfully")
+        return redirect('Home')
+    else:
+        messages.success(request, 'You must be Logged in to do that')
+        return redirect('Home')
 
