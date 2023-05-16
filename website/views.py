b5f5e76183c0df07128b26e9cadd3896ce6fedd8
MarvsConcept <marvade8@gmail.com>
2023-05-16 02:12:12 +0100

    Final Project

--- website/views.py
+++ website/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
-from .forms import SignUpForm
+from .forms import SignUpForm, AddRecordForm
 from .models import Record
 # Create your views here.
 def home(request):
@@ -66,4 +66,29 @@ def delete_record(request, pk):
     else:
         messages.success(request, 'You must be Logged in to do that')
         return redirect('Home')
+    
+def add_record(request):
+    form = AddRecordForm(request.POST or None)
+    if request.user.is_authenticated:
+        if request.method == "POST":
+            if form.is_valid():
+                add_record = form.save()
+                messages.success(request, "Record Added successfully...")
+                return redirect("Home")
+        return render(request, 'add_record.html', {'form':form})
+    else:
+        messages.success(request, "You must be logged in!")
+        return redirect ("Home")
 
+def update_record(request, pk):
+    if request.user.is_authenticated:
+        current_record = Record.objects.get(id=pk)
+        form = AddRecordForm(request.POST or None, instance=current_record)
+        if form.is_valid():
+            form.save()
+            messages.success(request, "Record Updated Successfully")
+            return redirect("Home")
+        return render(request, 'update_record.html', {'form':form})
+    else:
+        messages.success(request, "you must be logged in...")
+        return redirect("Home")
