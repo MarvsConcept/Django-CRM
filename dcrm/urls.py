a2411d92c7b56c27aac118dbfcacc931b0155f6e
MarvsConcept <Marvade8@gmail.com>
2023-12-30 04:23:30 +0100

    Added Configurations for Vercel

--- dcrm/urls.py
+++ dcrm/urls.py
@@ -22,5 +22,7 @@ from django.conf import settings
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('website.urls')),
-]   + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) \
-    + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+]   
+
+static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) 
+static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
