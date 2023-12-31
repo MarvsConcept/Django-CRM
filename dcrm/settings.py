809cb1454bbd22536dab8ce4182073ca1cd135ee
MarvsConcept <Marvade8@gmail.com>
2023-12-31 21:45:18 +0100

    update

--- dcrm/settings.py
+++ dcrm/settings.py
@@ -65,7 +65,7 @@ ROOT_URLCONF = 'dcrm.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR/'templates'],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
